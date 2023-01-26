82ad391c09bc26e70b28b88cec77dd555c47ce21
mattheusmiranda <matheusrod59@gmail.com>
2023-01-26 13:46:58 -0300

    primeiro commit

---
+++ src/test/java/com/br/bookstore2/Bookstore2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.bookstore2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bookstore2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
